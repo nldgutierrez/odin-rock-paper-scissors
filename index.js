46fdd4ac3c6a31cb0d07dabafa03a7ca6ce4c5c9
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2023-02-08 07:51:14 +0800

    Add prompt to start the game and playerSelection

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log("Hello, world!");
 
+// Prompt to start the game
+if (confirm("Are you ready to play the game?")) {
+    let playerSelection = prompt("Pick your weapon! (Rock, Paper, or Scissors)");
+    console.log(playerSelection);
+}
