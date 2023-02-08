58d3234cd37a334619f869dfda1d425009354fba
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2023-02-08 08:00:27 +0800

    Create function getComputerChoice()

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 console.log("Hello, world!");
 
 // Prompt to start the game
-if (confirm("Are you ready to play the game?")) {
-    let playerSelection = prompt("Pick your weapon! (Rock, Paper, or Scissors)");
-    console.log(playerSelection);
-}
+// if (confirm("Are you ready to play the game?")) {
+//     let playerSelection = prompt("Pick your weapon! (Rock, Paper, or Scissors)");
+//     console.log(playerSelection);
+// }
+
+const playerSelection = "rock";
+const choices = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+    let x;
+    x = choices[Math.floor(Math.random()*choices.length)];
+    return x;
+};
+
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
