696b6bc1aae344346ae03cf82051098bc15dfb5e
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2023-02-08 09:50:07 +0800

    Create function playRound()
    Create function playRound() that compares user input with computer
    selection and returns a string accordingly.

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ function getComputerChoice() {
 
 let computer = getComputerChoice();
 console.log(computer);
+
+function playRound(playerSelection, computerSelection) {
+
+    switch (true) {
+        case playerSelection == computerSelection:
+            return "It's a tie.";
+
+        case (
+            (playerSelection == "Rock" && computerSelection == "Scissors") ||
+            (playerSelection == "Paper" && computerSelection == "Rock") ||
+            (playerSelection == "Scissors" && computerSelection == "Paper")
+        ):
+            return `You win! ${playerSelection} beats ${computerSelection}.`
+    
+        default:
+            return `You lose! ${computerSelection} beats ${playerSelection}.`
+    }
+}
+
+console.log(playRound(player, computer))
