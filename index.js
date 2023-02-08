8cecabd8c025b5664aabeb564643a260382cf018
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2023-02-08 09:22:30 +0800

    Delete test code

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-console.log("Hello, world!");
-
-// Prompt to start the game
-// if (confirm("Are you ready to play the game?")) {
-//     let playerSelection = prompt("Pick your weapon! (Rock, Paper, or Scissors)");
-//     console.log(playerSelection);
-// }
-
 let player = prompt("Choose your fighter!");
 player = player.toLowerCase();
 player = player[0].toUpperCase()+player.substring(1);
