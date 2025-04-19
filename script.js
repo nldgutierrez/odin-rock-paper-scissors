7dbb425ec25a10a5ef6a1b69481fc41d3ca69135
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2025-04-19 12:53:05 +0800

    Initialize variables

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const choices = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
 };
+
+let humanScore = 0;
+let computerScore = 0;
+
+const output = document.querySelector("#gameplay");
+const human = document.createElement("p");
+const computer = document.createElement("p");
+const result = document.createElement("p");
+const scores = document.createElement("p");
