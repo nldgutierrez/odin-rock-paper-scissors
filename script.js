3ab7a27b0ee867b16aebcaa977f6b62738abaf76
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2025-04-19 12:54:30 +0800

    Create function to play one round

--- script.js
+++ script.js
@@ -12,3 +12,54 @@ const human = document.createElement("p");
 const computer = document.createElement("p");
 const result = document.createElement("p");
 const scores = document.createElement("p");
+
+function playRound(humanChoice, computerChoice) {
+
+    humanChoice = humanChoice[0].toUpperCase()+humanChoice.substr(1);
+
+    switch (true) {
+        case (humanChoice == computerChoice):
+            human.textContent = `You chose ${humanChoice}.`;
+            computer.textContent = `Computer chose ${computerChoice}.`;
+            result.textContent = `It's a tie!`;
+            scores.textContent = `You -- ${humanScore} | Computer -- ${computerScore}`;
+        
+            output.appendChild(human);
+            output.appendChild(computer);
+            output.appendChild(result);
+            output.appendChild(scores);
+            break;
+
+        case (
+            humanChoice == 'Rock' && computerChoice == 'Scissors' ||
+            humanChoice == 'Paper' && computerChoice == 'Rock' ||
+            humanChoice == 'Scissors' && computerChoice == 'Paper'
+        ):
+            human.textContent = `You chose ${humanChoice}.`;
+            computer.textContent = `Computer chose ${computerChoice}.`;
+            result.textContent = `You win!`;
+
+            humanScore++;
+
+            scores.textContent = `You -- ${humanScore} | Computer -- ${computerScore}`;
+        
+            output.appendChild(human);
+            output.appendChild(computer);
+            output.appendChild(result);
+            output.appendChild(scores);
+            break;
+        default:
+            human.textContent = `You chose ${humanChoice}.`;
+            computer.textContent = `Computer chose ${computerChoice}.`;
+            result.textContent = `You lose!`;
+
+            computerScore++;
+
+            scores.textContent = `You -- ${humanScore} | Computer -- ${computerScore}`;
+        
+            output.appendChild(human);
+            output.appendChild(computer);
+            output.appendChild(result);
+            output.appendChild(scores);
+    }
+}
